a427e7b903536b09c324013530affd1f0720f504
Maxw2 <635433387@qq.com>
2019-06-20 01:18:59 +0800

    Updates

---
+++ precache-manifest.1556016e4c209085c46081ceec49dbb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e88c5fdfe31766b2772794be9f1b13",
+    "url": "/create-react-app/index.html"
+  },
+  {
+    "revision": "052e528dcc38820b39b2",
+    "url": "/create-react-app/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/create-react-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "052e528dcc38820b39b2",
+    "url": "/create-react-app/static/js/main.10ff1247.chunk.js"
+  },
+  {
+    "revision": "ee6d20426387d02678ad",
+    "url": "/create-react-app/static/js/runtime~main.7a134d5e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/create-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
